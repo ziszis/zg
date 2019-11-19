f40bc78e659e888a4c2cb9764f885d0d53fa4369
Ivan Zauharodneu <zis.zis@gmail.com>
2019-11-19 18:53:27 +0100

    Faster line splitting

--- zg.cc
+++ zg.cc
@@ -58,14 +58,48 @@ void SplitLine(std::string_view line, std::vector<FieldValue>* fields) {
   }
 }
 
+template<class LineFn>
+void ForEachInputLine(LineFn lineFn) {
+  std::string buffer(16384, '\0');
+  char* begin = buffer.data();
+  size_t bufferLeft = 0;
+
+  while (true) {
+    size_t wantToRead = buffer.size() - bufferLeft;
+    size_t actuallyRead = std::fread(begin + bufferLeft, 1, wantToRead, stdin);
+    bufferLeft += actuallyRead;
+    while (true) {
+      char* p = static_cast<char*>(memchr(begin, '\n', bufferLeft));
+      if (p != nullptr) {
+        lineFn(std::string_view(begin, p - begin));
+        ++p;
+        bufferLeft -= p - begin;
+        begin = p;
+      } else if (wantToRead == actuallyRead) {
+        memmove(buffer.data(), begin, bufferLeft);
+        if (bufferLeft > buffer.size() / 4) {
+          buffer.resize(buffer.size() * 4, '\0');
+        }
+        begin = buffer.data();
+        // TODO: Shrink buffer if too little of it is used?
+        break;
+      } else {
+        if (bufferLeft != 0) {
+          lineFn(std::string_view(begin, bufferLeft));
+        }
+        return;
+      }
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
-  std::string line;
   std::vector<FieldValue> fields;
   Data data;
-  while (std::getline(std::cin, line)) {
+  ForEachInputLine([&](std::string_view line) {
     SplitLine(line, &fields);
     data.Process(fields);
-  }
+  });
   data.Render(std::cout);
   return 0;
 }
