f2ff1285ad8e0fb90551257e9c3c3f010c909c85
Ivan Zauharodneu <zis.zis@gmail.com>
2019-11-19 18:53:35 +0100

    Basic aggregators

--- zg.cc
+++ zg.cc
@@ -7,6 +7,7 @@
 
 #include "absl/strings/numbers.h"
 #include "absl/strings/str_split.h"
+#include "absl/container/flat_hash_map.h"
 
 void Fail(std::string_view reason) {
   std::cerr << reason << std::endl;
@@ -34,18 +35,80 @@ class FieldValue {
   mutable std::optional<int64_t> int_;
 };
 
+class Aggregator {
+ public:
+  virtual ~Aggregator() {}
+  virtual size_t StateSize() const = 0;
+  virtual void Init(char* state) = 0;
+  virtual void Push(const FieldValue&, char* state) = 0;
+  virtual void Print(const char* state, std::string* out) const = 0;
+};
+
+class CountAggregator : public Aggregator {
+  size_t StateSize() const override { return 8; }
+
+  void Init(char* state) override {
+    *reinterpret_cast<int64_t*>(state) = 0;
+  }
+
+  void Push(const FieldValue&, char* state) override {
+    ++(*reinterpret_cast<int64_t*>(state));
+  }
+
+  void Print(const char* state, std::string* out) const override {
+    absl::StrAppend(out, *reinterpret_cast<const int64_t*>(state));
+  }
+};
+
+class SampleAggregator : public Aggregator {
+  size_t StateSize() const override { return 4; }
+  void Init(char* state) override {}
+  void Push(const FieldValue&, char* state) override {}
+  void Print(const char* state, std::string* out) const override {}
+};
+
 class Data {
  public:
   Data() {
+    agg_.push_back(std::make_unique<CountAggregator>());
+    state_offset_.push_back(0);
   }
 
   void Process(const std::vector<FieldValue>& fields) {
+    auto [it, inserted] = rows_.try_emplace(fields[1].AsString());
+    char* row = it->second;
+    if (inserted) {
+      agg_[0]->Init(row);
+    }
+    agg_[0]->Push(fields[1], row);
   }
 
-  void Render(std::ostream& os) {
+  void Render() {
+    std::string buffer;
+    for (const auto& [key, value] : rows_) {
+      buffer.append(key);
+      buffer.push_back('\t');
+      const char* row = value;
+      agg_[0]->Print(row, &buffer);
+      buffer.push_back('\n');
+      if (buffer.size() > 1 << 15) {
+        if (std::fwrite(buffer.data(), 1, buffer.size(), stdout) != buffer.size()) {
+          Fail("Write failed");
+        }
+        buffer.clear();
+      }
+    }
+    if (std::fwrite(buffer.data(), 1, buffer.size(), stdout) != buffer.size()) {
+      Fail("Write failed");
+    }
   }
 
  private:
+  using Row = char[8];
+  absl::flat_hash_map<std::string, Row> rows_;
+
+  std::vector<size_t> state_offset_;
+  std::vector<std::unique_ptr<Aggregator>> agg_;
 };
 
 void SplitLine(std::string_view line, std::vector<FieldValue>* fields) {
@@ -100,6 +163,6 @@ int main(int argc, char *argv[]) {
     SplitLine(line, &fields);
     data.Process(fields);
   });
-  data.Render(std::cout);
+  data.Render();
   return 0;
 }
